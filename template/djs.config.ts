6cd537dbbccee4bdf63f392d9a88ff8719b56295
fakejsdev <fakejsdev@proton.me>
2026-08-30 18:13:17 +0200

    Format djs.config.ts for improved readability

--- template/djs.config.ts
+++ template/djs.config.ts
@@ -2,23 +2,23 @@ import { ActivityType, GatewayIntentBits, Partials } from "discord.js";
 import { defineConfig } from "@/lib/helpers/defineConfig";
 
 export const config = defineConfig({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-  partials: [Partials.Channel, Partials.GuildMember, Partials.Message],
-  modulesDir: "src/modules",
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+	partials: [Partials.Channel, Partials.GuildMember, Partials.Message],
+	modulesDir: "src/modules",
 
-  handlers: { all: true },
+	handlers: { all: true },
 
-  presence: {
-    status: "online",
-    activity: {
-      type: ActivityType.Playing,
-      name: "with Djs Toolkit",
-    },
-  },
+	presence: {
+		status: "online",
+		activity: {
+			type: ActivityType.Playing,
+			name: "with Djs Toolkit",
+		},
+	},
 
-  logLevel: "debug",
+	logLevel: "debug",
 });
