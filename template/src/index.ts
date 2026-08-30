885b30e774a38fe7057576561bfe85cc0af2a49a
fakejsdev <fakejsdev@proton.me>
2026-08-30 21:59:57 +0200

    Implement client event handlers and logging for Discord bot

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import { config } from "@config";
+import { initHandlers } from "@/handlers";
+import { client } from "@/lib/discord";
+import { Console } from "@/lib/logger";
+
+client.on("clientReady", async (client) => {
+	await initHandlers(config);
+	Console.Log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("error", (error) => {
+	config.onError?.(error, { source: "gateway", name: "client" });
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
