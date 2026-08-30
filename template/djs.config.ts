a1d43f3d8cb802ad266273bca8b51a05f4469dc1
fakejsdev <fakejsdev@proton.me>
2026-08-30 02:59:59 +0200

    Add initial configuration for Discord.js in djs.config.ts

---
+++ template/djs.config.ts
@@ -0,0 +1,24 @@
+import { ActivityType, GatewayIntentBits, Partials } from "discord.js";
+import { defineConfig } from "@/lib/helpers/defineConfig";
+
+export const config = defineConfig({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [Partials.Channel, Partials.GuildMember, Partials.Message],
+  modulesDir: "src/modules",
+
+  handlers: { all: true },
+
+  presence: {
+    status: "online",
+    activity: {
+      type: ActivityType.Playing,
+      name: "with Djs Toolkit",
+    },
+  },
+
+  logLevel: "debug",
+});
