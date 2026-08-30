df8c6b624b3a6768904ca04a2ee81deca1209bc4
fakejsdev <fakejsdev@proton.me>
2026-08-30 22:46:44 +0200

    Refactor defineCommand to include permissions handling and resolve default_member_permissions

--- template/src/lib/helpers/defineCommand.ts
+++ template/src/lib/helpers/defineCommand.ts
@@ -1,11 +1,22 @@
 import type { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { type PermissionFlag, resolvePermissions } from "@/lib/discord/permissions";
 
 type CommandInteraction = ChatInputCommandInteraction<"cached">;
+type CommandJSON = ReturnType<SlashCommandBuilder["toJSON"]>;
 
-export type CommandConfig = ReturnType<SlashCommandBuilder["toJSON"]>;
+export interface CommandConfig extends Omit<CommandJSON, "default_member_permissions"> {
+  permissions?: PermissionFlag[];
+}
 export type CommandRun = (i: CommandInteraction) => unknown | Promise<unknown>;
 
-export const defineCommand = (config: CommandConfig, run: CommandRun) => ({
-  config,
-  run,
-});
+export const defineCommand = (config: CommandConfig, run: CommandRun) => {
+  const { permissions, ...rest } = config;
+
+  return {
+    config: {
+      ...rest,
+      default_member_permissions: resolvePermissions(permissions),
+    },
+    run,
+  };
+};
