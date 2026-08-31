8c6557f29c074e7c759eb624e51855f697f58a17
fakejsdev <fakejsdev@proton.me>
2026-08-31 04:45:51 +0200

    Add event handler initialization to initHandlers function

--- template/src/handlers/index.ts
+++ template/src/handlers/index.ts
@@ -1,6 +1,8 @@
 import type { ResolvedDjsConfig } from "@/lib/helpers/defineConfig";
 import { initCommandHandler } from "./commands/commandsHandler";
+import { initEventHandler } from "./events/eventsHandler";
 
 export const initHandlers = async (config: ResolvedDjsConfig) => {
   if (config.handlers.commands) await initCommandHandler();
+  if (config.handlers.events) await initEventHandler();
 };
