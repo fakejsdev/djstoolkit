2aafcf11a90956a80039f471278ad7f6a39b88f3
fakejsdev <fakejsdev@proton.me>
2026-09-07 07:42:30 +0200

    Add initialization for dropdown handler in components handler

--- template/src/handlers/components/index.ts
+++ template/src/handlers/components/index.ts
@@ -1,5 +1,7 @@
 import { initButtonHandler } from "./buttonsHandler";
+import { initDropdownHandler } from "./dropdownHandler";
 
 export const initComponentsHandler = async () => {
   await initButtonHandler();
+  await initDropdownHandler();
 };
