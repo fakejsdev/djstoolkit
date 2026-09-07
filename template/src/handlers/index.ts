8eea2a88b9850a732303d28d48bccd6179adfe75
fakejsdev <fakejsdev@proton.me>
2026-09-07 08:53:11 +0200

    Add database event handler initialization to handler setup

--- template/src/handlers/index.ts
+++ template/src/handlers/index.ts
@@ -1,10 +1,12 @@
 import type { ResolvedDjsConfig } from "@/lib/helpers/defineConfig";
 import { initCommandHandler } from "./commands/commandsHandler";
 import { initComponentsHandler } from "./components";
+import { initDbEventHandler } from "./db/dbEventsHandler";
 import { initEventHandler } from "./events/eventsHandler";
 
 export const initHandlers = async (config: ResolvedDjsConfig) => {
   if (config.handlers.commands) await initCommandHandler();
   if (config.handlers.events) await initEventHandler();
+  if (config.handlers.databaseEvents) await initDbEventHandler();
   if (config.handlers.components) await initComponentsHandler();
 };
