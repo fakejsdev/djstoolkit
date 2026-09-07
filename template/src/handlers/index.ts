6d21e409ada0bc305f1390d371aaad60f477ecd1
fakejsdev <fakejsdev@proton.me>
2026-09-07 06:10:43 +0200

    Add component handler initialization to initHandlers function

--- template/src/handlers/index.ts
+++ template/src/handlers/index.ts
@@ -1,8 +1,10 @@
 import type { ResolvedDjsConfig } from "@/lib/helpers/defineConfig";
 import { initCommandHandler } from "./commands/commandsHandler";
+import { initComponentsHandler } from "./components";
 import { initEventHandler } from "./events/eventsHandler";
 
 export const initHandlers = async (config: ResolvedDjsConfig) => {
   if (config.handlers.commands) await initCommandHandler();
   if (config.handlers.events) await initEventHandler();
+  if (config.handlers.components) await initComponentsHandler();
 };
