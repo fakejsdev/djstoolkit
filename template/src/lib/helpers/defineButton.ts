0a013637290ffca654ffe782a059a23863bdca1d
fakejsdev <fakejsdev@proton.me>
2026-09-07 07:11:05 +0200

    Rename ButtonConfig property 'id' to 'customId' for clarity

--- template/src/lib/helpers/defineButton.ts
+++ template/src/lib/helpers/defineButton.ts
@@ -3,11 +3,10 @@ import type { ButtonInteraction as DiscordButtonInteraction } from "discord.js";
 type ButtonInteraction = DiscordButtonInteraction<"cached">;
 
 export type ButtonConfig = {
-  id: string;
+  customId: string;
   name: string;
   description: string;
 };
-
 export type ButtonRun = (i: ButtonInteraction) => unknown | Promise<unknown>;
 
 export const defineButton = (config: ButtonConfig, run: ButtonRun) => ({ config, run });
