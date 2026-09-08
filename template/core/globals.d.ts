eb34210eead99ac33e7cb50b402737e0c426ca5b
fakejsdev <fakejsdev@proton.me>
2026-09-08 17:33:41 +0200

    refactor: remove create-djstoolkit package and update environment type definitions

--- template/core/globals.d.ts
+++ template/core/globals.d.ts
@@ -1,11 +1,7 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      DISCORD_APP_ID: string;
-      DISCORD_BOT_TOKEN: string;
-      DEV_GUILD_ID?: string;
-    }
+namespace NodeJS {
+  interface ProcessEnv {
+    DISCORD_APP_ID: string;
+    DISCORD_BOT_TOKEN: string;
+    DEV_GUILD_ID?: string;
   }
 }
-
-export {};
