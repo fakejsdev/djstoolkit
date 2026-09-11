bc6c16139539b580eed6a2bf050fcfeb3ef61851
fakejsdev <fakejsdev@proton.me>
2026-09-11 18:33:19 +0200

    feat: implement scaffold function for setting up project features and dependencies

--- packages/create-djstoolkit/src/scaffold/index.ts
+++ packages/create-djstoolkit/src/scaffold/index.ts
@@ -1,3 +1,32 @@
+import { log, spinner } from "@clack/prompts";
+import { FEATURES } from "@/features/registry";
 import type { Answers } from "@/prompts";
+import { gitInit } from "@/tasks/gitInit";
+import { installDeps } from "@/tasks/installDeps";
+import { copyTemplate } from "@/utils/copyTemplate";
 
-export const scaffold = (answers: Answers) => {};
+export const scaffold = async (answers: Answers) => {
+  const targetDir = answers.name;
+
+  log.step("Copying Core Features...");
+  copyTemplate(targetDir);
+
+  for (const feature of FEATURES.filter((f) => answers.features.includes(f.id))) {
+    log.step(`Installing ${feature.label}...`);
+    await feature.installer(targetDir, answers);
+  }
+
+  if (answers.installDependencies) {
+    const s = spinner();
+    s.start("Installing dependencies...");
+    await installDeps(targetDir);
+    s.stop("Installed successfully!");
+  }
+
+  if (answers.gitInit) {
+    log.step("Initializing Git repository...");
+    await gitInit(targetDir);
+  }
+
+  log.success("🧩 Setup complete!");
+};
