c4536d399770af4573f0235a28eb414106e0a980
fakejsdev <fakejsdev@proton.me>
2026-09-11 15:43:47 +0200

    feat: refactor globals.d.ts to declare global namespace for ProcessEnv

--- template/core/globals.d.ts
+++ template/core/globals.d.ts
@@ -1,7 +1,11 @@
-namespace NodeJS {
-  interface ProcessEnv {
-    DISCORD_APP_ID: string;
-    DISCORD_BOT_TOKEN: string;
-    DEV_GUILD_ID?: string;
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DISCORD_APP_ID: string;
+      DISCORD_BOT_TOKEN: string;
+      DEV_GUILD_ID?: string;
+    }
   }
 }
