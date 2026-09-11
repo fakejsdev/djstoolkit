313116ba956ddd920019ed8e565a11d3d305da79
fakejsdev <fakejsdev@proton.me>
2026-09-11 20:07:54 +0200

    fix: update queueName in bullmq configuration to DJSTOOLKIT_WORK_QUEUE

--- template/features/bullmq/djs.config.ts
+++ template/features/bullmq/djs.config.ts
@@ -3,5 +3,5 @@ import type { DjsConfig } from "@/lib/helpers/defineConfig";
 export const bullmqConfig: DjsConfig["bullmq"] = {
   hostname: process.env.REDIS_HOSTNAME ?? "localhost",
   port: Number(process.env.REDIS_PORT ?? 6379),
-  queueName: "default",
+  queueName: "DJSTOOLKIT_WORK_QUEUE",
 };
