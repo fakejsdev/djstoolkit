ece54df7ba544f1b2ba0418dc17d00322740d6ca
fakejsdev <fakejsdev@proton.me>
2026-09-11 19:22:58 +0200

    feat: implement updatePackageJson utility function for managing package.json dependencies and scripts

--- packages/create-djstoolkit/src/utils/updatePackageJson.ts
+++ packages/create-djstoolkit/src/utils/updatePackageJson.ts
@@ -1,9 +1,16 @@
 import { join } from "node:path";
 
-export const addPackageDependency = async (
+export const updatePackageJson = async (
   targetDir: string,
-  dependencies?: Record<string, string>,
-  devDependencies?: Record<string, string>,
+  {
+    dependencies,
+    devDependencies,
+    scripts,
+  }: {
+    dependencies?: Record<string, string>;
+    devDependencies?: Record<string, string>;
+    scripts?: Record<string, string>;
+  },
 ) => {
   const pkgPath = join(targetDir, "package.json");
   const pkg = await Bun.file(pkgPath).json();
@@ -12,10 +19,13 @@ export const addPackageDependency = async (
     pkg.dependencies ??= {};
     Object.assign(pkg.dependencies, dependencies);
   }
-
   if (devDependencies) {
     pkg.devDependencies ??= {};
     Object.assign(pkg.devDependencies, devDependencies);
+  }
+  if (scripts) {
+    pkg.scripts ??= {};
+    Object.assign(pkg.scripts, scripts);
   }
 
   await Bun.write(pkgPath, JSON.stringify(pkg, null, 2));
