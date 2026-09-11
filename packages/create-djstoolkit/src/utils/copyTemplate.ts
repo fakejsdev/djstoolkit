655aa11233950150103f04b3e37ccbaf61718823
fake js developer <fakejsdev@proton.me>
2026-09-11 20:14:16 +0200

    fix: update package version to 0.2.1 and improve template exclusion logic (#2)

--- packages/create-djstoolkit/src/utils/copyTemplate.ts
+++ packages/create-djstoolkit/src/utils/copyTemplate.ts
@@ -1,14 +1,17 @@
 import { cpSync, existsSync } from "node:fs";
-import { join } from "node:path";
+import { join, relative } from "node:path";
 import { resolveGitignorePath } from "./resolveGitignorePaths";
 
 const DEV_TEMPLATE_PATH = join(import.meta.dir, "../../../../template");
 const PROD_TEMPLATE_PATH = join(import.meta.dir, "../template");
 export const TEMPLATE_DIR = existsSync(DEV_TEMPLATE_PATH) ? DEV_TEMPLATE_PATH : PROD_TEMPLATE_PATH;
 
-const isExcluded = (src: string) => EXCLUDED_FILES.some((excluded) => src.includes(excluded));
-
 const EXCLUDED_FILES = ["node_modules", ".gitignore", "gitignore"];
+
+const isExcluded = (src: string) => {
+  const relativePath = relative(join(TEMPLATE_DIR, "core"), src);
+  return EXCLUDED_FILES.some((excluded) => relativePath.includes(excluded));
+};
 
 export const copyTemplate = (targetDir: string) => {
   cpSync(join(TEMPLATE_DIR, "core"), targetDir, {
