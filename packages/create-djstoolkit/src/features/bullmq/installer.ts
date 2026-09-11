ad6e23b17ab86fcf51fca37503b12fb741d66999
fakejsdev <fakejsdev@proton.me>
2026-09-11 19:22:45 +0200

    fix: replace addPackageDependency with updatePackageJson in installBullmq function

--- packages/create-djstoolkit/src/features/bullmq/installer.ts
+++ packages/create-djstoolkit/src/features/bullmq/installer.ts
@@ -1,12 +1,12 @@
 import { cpSync } from "node:fs";
 import { join } from "node:path";
 import type { Answers } from "@/prompts";
-import { addPackageDependency } from "@/utils/addPackageDependency";
 import { appendGlobalsDts } from "@/utils/appendGlobalsDts";
 import { appendEnvExample } from "@/utils/appendToFile";
 import { TEMPLATE_DIR } from "@/utils/copyTemplate";
 import { injectConfigFragment } from "@/utils/injectConfigFragment";
 import { mergeDockerComposeService } from "@/utils/mergeDockerComposeService";
+import { updatePackageJson } from "@/utils/updatePackageJson";
 import { dependencies } from "./dependencies";
 
 export const installBullmq = async (targetDir: string, answers: Answers) => {
@@ -14,7 +14,7 @@ export const installBullmq = async (targetDir: string, answers: Answers) => {
 
   cpSync(join(featureDir, "src"), join(targetDir, "src"), { recursive: true });
 
-  await addPackageDependency(targetDir, dependencies);
+  await updatePackageJson(targetDir, { dependencies });
   await appendEnvExample(targetDir, join(featureDir, ".env.example"));
   await appendGlobalsDts(targetDir, join(featureDir, "bullmq.d.ts"));
   await injectConfigFragment(targetDir, "bullmq", join(featureDir, "djs.config.ts"));
