b6834071dcd75a742f1611d0b4dce902e06e16a2
fakejsdev <fakejsdev@proton.me>
2026-09-17 22:55:54 +0200

    feat: add copyRelayTemplate function for copying relay template files

---
+++ packages/create-djstoolkit/src/features/relay/template.ts
@@ -0,0 +1,9 @@
+import { cpSync } from "node:fs";
+import { join } from "node:path";
+import { TEMPLATE_DIR } from "@/utils/copyTemplate";
+
+export const copyRelayTemplate = (targetDir: string) => {
+  const templateDir = join(TEMPLATE_DIR, "examples", "example-relay");
+
+  cpSync(join(templateDir, "src"), join(targetDir, "src"), { recursive: true, force: true });
+};
